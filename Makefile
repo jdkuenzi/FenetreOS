2754b466cddcc5dfbbe031dd5b0ba5cd77237874
jeandani.kuenzi <jean-daniel.kuenzi@etu.hesge.ch>
2021-01-29 18:00:27 +0100

    Add logo, image and PMIO

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 QEMU=qemu-system-i386 -enable-kvm -m 64 -monitor stdio
-BUILD_STRUCTURE=build/boot/grub build/dat
-KERNEL_FOLDER=build/
+BUILD_FOLDER=build/
+BUILD_STRUCTURE=$(BUILD_FOLDER)/boot/grub $(BUILD_FOLDER)/dat
+MODULE_SRC=res/
+MODULE_DST=build/dat/
 GRUB_CFG_SRC=grub/grub.cfg
 GRUB_CFG_DST=build/boot/grub/
 ISO_NAME=fenetre.iso
@@ -15,11 +17,12 @@ run_shell: kernel
 
 $(ISO_PATH):
 	mkdir $(ISO_FOLDER)
-	grub-mkrescue /usr/lib/grub/i386-pc $(KERNEL_FOLDER) -o $@
+	grub-mkrescue /usr/lib/grub/i386-pc $(BUILD_FOLDER) -o $@
 
 kernel:
 	mkdir -p $(BUILD_STRUCTURE)
 	cp $(GRUB_CFG_SRC) $(GRUB_CFG_DST)
+	cp -r $(MODULE_SRC) $(MODULE_DST)
 	$(MAKE) -C kernel
 
 .PHONY: kernel clean
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,6 @@
 #include "../common/types.h"
 #include "boot/multiboot.h"
+#include "pmio/pmio.h"
 
 // These are defined in the linker script: kernel.ld
 extern void ld_kernel_start();
@@ -7,26 +8,37 @@ extern void ld_kernel_end();
 uint_t kernel_start = (uint_t)&ld_kernel_start;
 uint_t kernel_end = (uint_t)&ld_kernel_end;
 
+void clean_vga(char *vidptr) {
+	int i = 0;
+	while(i < 80 * 25 * 2) {
+		vidptr[i] = ' ';
+		vidptr[i+1] = 0x07; 		
+		i += 2;
+	}
+}
+
+void write_string(char *vidptr, char *str) {
+
+	int j = 0;
+	while(*str) {
+		vidptr[j] = *str;
+		vidptr[j+1] = 0x07;
+		str++;
+		j += 2;
+	}
+}
+
+void my_printf() {
+	// TO DO
+}
 
 void entry(void)
 {
-	const char *str = "Je suis la fenetre";
+	const char *str = "Je suis la fenetre :)";
 	char *vidptr = (char*)0xb8000;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	while(j < 80 * 25 * 2) {
-		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07; 		
-		j = j + 2;
-	}
-
-	j = 0;
-
-	while(str[j] != '\0') {
-		vidptr[i] = str[j];
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
-	}
+	clean_vga(vidptr);
+	write_string(vidptr, str);
 	return;
 }
