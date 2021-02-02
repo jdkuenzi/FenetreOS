d8c2e3ce72bba95fe5d54ca0e00ca13c05861741
ottavio.buonomo <ottavio.buonomo@etu.hesge.ch>
2021-02-02 08:27:31 -0800

    Debut printf

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,15 @@
 #include "boot/multiboot.h"
 #include "pmio/pmio.h"
 
+#define COLONNES 80
+#define LIGNES 25
+#define VGA 0xB8000
+
+int cursor_x = 10;
+int cursor_y = 10;
+
+uint16_t *screen;
+
 // These are defined in the linker script: kernel.ld
 extern void ld_kernel_start();
 extern void ld_kernel_end();
@@ -17,7 +26,7 @@ void clean_vga(char *vidptr) {
 	}
 }
 
-void write_string(char *vidptr, char *str) {
+void write_string(char *vidptr, const char *str) {
 	int j = 0;
 	while(*str) {
 		vidptr[j] = *str;
@@ -27,18 +36,129 @@ void write_string(char *vidptr, char *str) {
 	}
 }
 
-void my_printf() {
-	// TO DO
+void printChar(uint8_t c) {
+
+	uint16_t *addr;
+
+	if (c == '\t')		// Tab
+	{
+		cursor_x += 4;
+	} else if (c == '\n')	// Return
+	{
+		cursor_x = 0;
+		cursor_y++;
+	} else if (c == 0x80)	// Backspace
+	{
+		cursor_x--;
+	} else if (c >= ' ')
+	{
+		char *vidptr = (char*)VGA;
+		clean_vga(vidptr);
+		write_string(vidptr, "JE SUIS");
+		addr = screen + (cursor_y * COLONNES + cursor_x);
+		//*addr = (0xF0<<8) | c;
+		cursor_x++;
+	}
+	
+	if (cursor_x >= COLONNES)	// Fin de ligne
+	{
+		cursor_x = 0;
+		cursor_y++;
+	}
 }
+
+// Permutation de deux valeurs
+void swap(char *x, char *y) {
+	char tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+// Inversion d'un buffer
+char* reverseBuffer(char *buffer, int i, int j)
+{
+    while (i < j) {
+		i++;
+		j--;
+        swap(&buffer[i], &buffer[j]);
+	}
+    return buffer;
+}
+
+void my_printf(char * fmt, ...) {
+	char **args = (char**) &fmt;
+	int c;
+	char buffer[50];
+
+	while ((c = *fmt++) != 0)
+	{
+		if (c != '%')
+		{
+			printChar(c);
+		} else 
+		{
+			char *p;
+			c = *fmt++;
+			switch (c)
+			{
+			case 'c':
+				/* code */
+				break;
+			case 's': {
+				p = *args++;
+				while (*p != NULL)
+				{
+					printChar(*p);
+					*p++;
+				}
+				
+				break;
+			}
+			case 'd':
+				/* code */
+				break;
+			case 'x': {
+				int n = *((int *) args++);
+				int i = 0;
+				while (n)
+				{
+					int res = n % 16;		// Base 16 -> HEX
+					if (res >= 10) 
+						buffer[i++] = 'a' + (res - 10);
+					else
+						buffer[i++] = '0' + res;
+					n = n / 16;
+				}
+				if (i == 0)
+        			buffer[i++] = '0';
+				buffer[i] = '\0';
+				char *newBuff = reverseBuffer(buffer, 0, i - 1);
+				while (*newBuff != NULL)
+				{
+					printChar(*newBuff);
+					*newBuff++;
+				}
+				break;
+			}
+			
+			default:
+				break;
+			}
+		}
+		
+	}
+	
+}
+
 
 void entry(void)
 {
+	screen = (unsigned short *)VGA;
 	const char *str = "Je suis la fenetre :)";
-	char *vidptr = (char*)0xb8000;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	char *vidptr = (char*)VGA;
 	clean_vga(vidptr);
 	write_string(vidptr, str);
+	printChar('P');
 	return;
 }
 
