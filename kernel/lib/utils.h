5ba8d339e93db19678e6e13bd2ffd384af1ec8f5
ottavio.buonomo <ottavio.buonomo@etu.hesge.ch>
2021-02-07 07:59:23 -0800

    Fix typo

--- kernel/lib/utils.h
+++ kernel/lib/utils.h
@@ -12,12 +12,6 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-/**
- * Program's entry point. 
- * @param num number of command line arguments.
- * @param base array of strings storing the command line arguments.
- * @return program exit code.
- */
 char *convert(unsigned int num, int base, char *buffer, unsigned int size);
 
 #endif
