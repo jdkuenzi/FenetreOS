33e4d623fcecee6a7adb91b807ae9189ae340c13
ottavio.buonomo <ottavio.buonomo@etu.hesge.ch>
2021-02-07 12:33:38 +0100

    Add comments for kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,14 @@
+/**
+ * @file kernel.c
+ * @brief Initial kernel in C
+ *
+ * @author Ottavio Buonomo & Jean-Daniel Küenzi,
+ * ottavio.buonomo@etu.hesge.ch & jean-daniel.kuenzi@etu.hesge.ch
+ * @bug No known bugs.
+ * @date February 7, 2021
+ * @version 0.1
+ */
+
 #include "boot/multiboot.h"
 #include "lib/stdio.h"
 
@@ -16,6 +27,7 @@ void entry(multiboot_info_t* info)
 {
 	init_vid(0x2, 0x0);
 
+	// Print of modules (Logo and image)
 	multiboot_module_t *mods_addr = (multiboot_module_t*)info->mods_addr;
 	for (multiboot_uint32_t i = 0; i < info->mods_count; i++) {
 		multiboot_uint32_t size = mods_addr->mod_end - mods_addr->mod_start + 1;
@@ -25,13 +37,14 @@ void entry(multiboot_info_t* info)
 		mods_addr++;
 	}
 
+	// Print of kernel infos
 	my_printf("Kernel loaded\n");
 	my_printf("	- addr: %x\n", kernel_start);
 	my_printf("	- size: %d [KB]\n", (kernel_end - kernel_start)/1000);
 	my_printf("	- RAM : %d [KB]\n", info->mem_upper);
-
 	my_printf("%d module(s) loaded\n", info->mods_count);
 
+	// Print of modules infos
 	mods_addr = (multiboot_module_t*)info->mods_addr;
 	for (multiboot_uint32_t i = 0; i < info->mods_count; i++) {
 		my_printf(
