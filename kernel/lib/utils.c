3b774ae255b71396b8612d676fc636f9125ad3c5
ottavio.buonomo <ottavio.buonomo@etu.hesge.ch>
2021-02-07 12:31:25 +0100

    Add comments for utils.c and final checkup

--- kernel/lib/utils.c
+++ kernel/lib/utils.c
@@ -1,7 +1,26 @@
+/**
+ * @file utils.c
+ * @brief utility functions library
+ *
+ * @author Ottavio Buonomo & Jean-Daniel Küenzi,
+ * ottavio.buonomo@etu.hesge.ch & jean-daniel.kuenzi@etu.hesge.ch
+ * @bug No known bugs.
+ * @date February 7, 2021
+ * @version 0.1
+ */
+
 #include "utils.h"
 
 // Inspiré d'un code internet
 // Ref : http://www.firmcodes.com/write-printf-function-c/
+/**
+ * Convert number to string 
+ * @param num number to convert
+ * @param base base to convert (ex: 10 (Dec), 16 (Hex))
+ * @param buffer buffer to fill
+ * @param size size of the buffer
+ * @return char* string buffer after conversion
+ */
 char *convert(unsigned int num, int base, char *buffer, unsigned int size) { 
 	static char representation[]= "0123456789ABCDEF";
 	
--- kernel/lib/utils.h
+++ kernel/lib/utils.h
@@ -1,3 +1,14 @@
+/**
+ * @file utils.h
+ * @brief utility functions library
+ *
+ * @author Ottavio Buonomo & Jean-Daniel Küenzi,
+ * ottavio.buonomo@etu.hesge.ch & jean-daniel.kuenzi@etu.hesge.ch
+ * @bug No known bugs.
+ * @date February 7, 2021
+ * @version 0.1
+ */
+
 #ifndef _UTILS_H
 #define _UTILS_H
 
