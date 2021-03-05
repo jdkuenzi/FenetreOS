2995a0659827aff3e1b45dddfc067d0b578e2b30
jeandani.kuenzi <jean-daniel.kuenzi@etu.hesge.ch>
2021-03-05 18:43:48 +0100

    Fix timer and add animated logo

--- kernel/drivers/timer.c
+++ kernel/drivers/timer.c
@@ -7,13 +7,42 @@
 
 static uint_t ticks;
 static uint32_t f_hz;
+static uint8_t loop_logo_ptr = 0;
+static char loop_logo[] = "---Fenetre_OS---";
+
+static void loop_logo_display() {
+	disable_cursor();
+
+	uint8_t tmp_ptr = loop_logo_ptr;
+	uint8_t size = strlen(loop_logo);
+	uint8_t x = COLONNES - size - 2; // -2 for '[' and ']' in my_prinft
+	uint8_t old_font_color = get_font_color();
+	uint16_t old_pos = get_cursor();
+	char reconstruct_loop_logo[size+1]; // size + 1 for \0
+	
+	set_font_color(COLOR_MAGENTA);
+	set_cursor_from_x_y(x, 0);
+	for (uint8_t i = 0; i < size; i++)
+	{
+		reconstruct_loop_logo[i] = loop_logo[tmp_ptr % size];
+		tmp_ptr++;
+	}
+	reconstruct_loop_logo[size] = '\0';
+	my_printf("[%s]", reconstruct_loop_logo);
+	set_cursor_from_pos(old_pos);
+	set_font_color(old_font_color);
+
+	enable_cursor();
+	loop_logo_ptr = loop_logo_ptr + 1 % size;
+}
 
 // Timer interrupt handler
 // Update ticks count and display a logo (animated)
 static void timer_handler() {
 	ticks++;
-	if (ticks%50 == 0) {
-		my_printf("\nFreq\n");
+	if (!(ticks % (f_hz/5))) // display all 0.20s
+	{
+		loop_logo_display();
 	}
 }
 
@@ -23,13 +52,14 @@ void timer_init(uint32_t freq_hz) {
 
 	uint16_t div = MAX_FREQ_HZ / freq_hz;
 
-	outw(CHAN_0, REPEAT_MODE);
+	outw(CMD_REG, REPEAT_MODE);
 	outb(CHAN_0, div & 0xff);
 	outb(CHAN_0, (div >> 8) & 0xff);
 	
 	// Install the timer interrupt handler
 	handler_t handler = { timer_handler, "timer" };
 	install_irq_handler(0, handler);
+	loop_logo_display();
 }
 
 uint_t get_ticks() {
@@ -37,9 +67,7 @@ uint_t get_ticks() {
 }
 
 void sleep(uint_t ms) {
-	uint_t count = f_hz * ms / 1000 + get_ticks();
-	while (get_ticks() < count)
-	{
-		/* nothing to do */
-	}
+	uint_t count = f_hz * ms / 1000;
+	count += get_ticks();
+	while (get_ticks() <= count) { /* nothing to do */ }
 }
