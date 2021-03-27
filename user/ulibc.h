714878bf6ca369b61a6001a1dcb8adf4e2cd82e5
jeandani.kuenzi <jean-daniel.kuenzi@etu.hesge.ch>
2021-03-27 18:34:13 +0100

    Update conflit

--- user/ulibc.h
+++ user/ulibc.h
@@ -3,7 +3,6 @@
 
 #include "../common/types.h"
 #include "../common/syscall_nb.h"
-<<<<<<< HEAD
 #include "../common/lib/string.h"
 #include "../common/lib/utils.h"
 
@@ -26,12 +25,5 @@ uint_t random(uint_t max);
 int cat(char *filename);
 int sleep(uint_t ms);
 void exit();
-=======
-
-extern int set_cursor_from_x_y(uint32_t x, uint32_t y);
-extern int puts(char* s);
-extern int my_printf(const char *fmt, ...);
-extern int syscall(syscall_t nb, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4);
->>>>>>> cd5dcb4d628bac7fd9c7f1b8404d803de924ba65
 
 #endif
